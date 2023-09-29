ad5fc494004c8f04fbeb72e748c101bb2dcd11e0
KSilva16 <contactkarinasilva@gmail.com>
2023-09-28 22:03:33 -0300

    fixing minor issue

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,12 @@
-module.exports ({
-  e2e: {
-    baseUrl: "https://www.atados.com.br/",
-    chromeWebSecurity: false,
-    setupNodeEvents(on, config) {
+module.exports = {
+    e2e: {
+      baseUrl: "https://www.atados.com.br/",
+      chromeWebSecurity: false,
+      setupNodeEvents(on, config) {
+        // Add any node event setup you need here
+      }
+    },
+    env: {
+      CYPRESS_INTERNAL_API_LOG: false
     }
-  },
-  env: {
-    CYPRESS_INTERNAL_API_LOG: false
   }
-});
